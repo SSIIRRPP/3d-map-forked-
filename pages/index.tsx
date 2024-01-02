3784c44fec14c5eb1195cf33321819580e5a1955
CodeSandbox <support@codesandbox.io>
2024-01-02 18:29:29 +0000

    Initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,11 @@ export default function Hola() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div id="canvas-container">
-        <Canvas>
+        <Canvas camera={}>
+          <ambientLight intensity={0.1} />
+          <directionalLight color="red" position={[0, 0, 5]} />
+          <ambientLight intensity={0.1} />
+          <directionalLight color="red" position={[0, 0, 5]} />
           <mesh>
             <boxGeometry />
             <meshStandardMaterial />
